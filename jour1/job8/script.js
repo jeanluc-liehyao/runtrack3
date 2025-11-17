22a18547e7bd35fbbd961d6f32f57bee3445034a
unknown <jean-luc.lieh-yao@laplateforme.io>
2025-11-17 17:02:13 +0100

    job 8 fini

---
+++ jour1/job8/script.js
@@ -0,0 +1,22 @@
+function estPremier(n) {
+    if (n < 2) return false;
+
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) return false;
+    }
+
+    return true;
+}
+
+function sommenombrespremiers(a, b) {
+    if (estPremier(a) && estPremier(b)) {
+        return a + b;
+    }
+    return false;
+}
+
+// Tests
+console.log(sommenombrespremiers(3, 5));
+console.log(sommenombrespremiers(7, 11));
+console.log(sommenombrespremiers(4, 7));
+console.log(sommenombrespremiers(8, 10));
